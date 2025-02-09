63c468318d658297d2169799ba75f6d61334007f
DiveshRampal <diveshrampal14@gmail.com>
2025-02-09 12:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+// Function to highlight bold words when mouse hovers over the link
 function highlight() {
-    //Write your code here
+  // Select all <strong> elements on the page
+  const strongElements = document.querySelectorAll('strong');
 
-
+  // Loop through each <strong> element and change the color to green
+  strongElements.forEach(function(element) {
+    element.style.color = 'rgb(0, 128, 0)'; // Green color
+  });
 }
 
-
+// Function to revert the color of bold words when mouse moves out of the link
 function return_normal() {
-    //Write your code here
+  // Select all <strong> elements on the page
+  const strongElements = document.querySelectorAll('strong');
 
-    
+  // Loop through each <strong> element and change the color back to black
+  strongElements.forEach(function(element) {
+    element.style.color = 'rgb(0, 0, 0)'; // Black color
+  });
 }
